304a0a7a3c3a63484881d917b36f5dea1754c685
Tane10 <tane2@hotmail.co.uk>
2024-06-26 13:42:54 +0100

    added linker to make file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
+void init_window(void){
+	if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+		fprintf(stderr, "Error init SDL... \n");
+		exit(0);
+	}
+}
 
 int main(){
-	printf("this is the main");
+	init_window();
+	// printf("this is the main");/
 	return 0;
 }
